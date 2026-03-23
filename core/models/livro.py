a81f28eb6a31c937ec883e294df7b979f112b19e
AmandaRibeiroLuz <amanda.ribeiro.da.luz.201@gmail.com>
2026-03-23 16:15:55 -0300

    FEAT: Inclusão do relacionamento n para n de livro e autor.

--- core/models/livro.py
+++ core/models/livro.py
@@ -3,6 +3,7 @@ from django.db import models
 
 from django.db.models.deletion import PROTECT
 
+from .autor import Autor
 from .categoria import Categoria
 from core.models import categoria
 from .editora import Editora
@@ -14,6 +15,7 @@ class Livro(models.Model):
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
+    autores = models.ManyToManyField(Autor, related_name='livros')
 
     def __str__(self):
         return f'({self.id}) {self.titulo} - Quantidade: {self.quantidade}'
