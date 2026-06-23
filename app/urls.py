7eaf7a1306174594fb07b0a7a998a31d45da2856
AmandaRibeiroLuz <amanda.ribeiro.da.luz.201@gmail.com>
2026-06-23 14:11:03 -0300

    FEAT: Correção no urls.py

--- app/urls.py
+++ app/urls.py
@@ -29,8 +29,8 @@ router = DefaultRouter()
 
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 router.register(r'autores', AutorViewSet, basename='autores')
-router.register(r'categorias', CategoriaViewSet, basename='categorias')
 router.register(r'compras', CompraViewSet, basename='compras')
+router.register(r'categorias', CategoriaViewSet, basename='categorias')
 router.register(r'editoras', EditoraViewSet, basename='editoras')
 router.register(r'livros', LivroViewSet, basename='livros')
 
