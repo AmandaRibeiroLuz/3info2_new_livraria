b91b9f70aaeaa4ea444af7af2a4f309dc3eafb64
AmandaRibeiroLuz <amanda.ribeiro.da.luz.201@gmail.com>
2026-06-23 14:09:18 -0300

    FEAT: Criação do endpoint para compras.

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,4 +1,5 @@
 from .autor import AutorSerializer
+from .compra import CompraSerializer
 from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
 from .livro import LivroListSerializer, LivroRetrieveSerializer, LivroSerializer
